079f6aac342db31721c80aa2698038f2c60d1e15
Vaibhav0-1 <vaibhav.sharma3100@gmail.com>
2024-10-06 23:52:30 +0530

    worker - signin endpoint

--- backend/src/routers/worker.ts
+++ backend/src/routers/worker.ts
@@ -2,13 +2,29 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client"
 import { JWT_SECRET} from "..";
+import { workerMiddleware } from "../middleware";
 
 export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
-
 
 const prismaClient = new PrismaClient();
 
 const router =  Router()
+
+router.get("/nexttask",workerMiddleware, async(req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+    
+    const Task = await prismaClient.task.findFirst({
+        where: {
+            user_id: Number(userId),
+            id: Number(taskId),
+        },
+        include: {
+            options: true
+        }
+    })
+
+})
 
 router.post("/signin", async(req,res)=>{
         //Todo: add sign verificartion logic here
